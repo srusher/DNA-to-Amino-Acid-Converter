01a58c4bcff25bd4106b27a67decaf70e23fcf56
Samuel Rusher <srusher@student.umuc.edu>
2020-10-04 20:43:45 -0700

    Project Files

---
+++ src/DNA.java
@@ -0,0 +1,239 @@
+/*
+Class Name: BIFS 618
+Homework assignment3, question1
+File name: hwk3_1.java
+Program author name: Samuel Rusher
+*/
+
+import java.lang.*;
+import java.util.*;
+
+
+public class DNA {
+
+    String dnaseq; //creating dnaseq field to store dna string
+
+    public static void codon(String dna){ //creating codon() method which accepts a string parameter
+
+        System.out.println("\nThe DNA sequence is:\n"+dna); //printing original dna sequence
+
+        //The segment below will find orf1
+
+        double count = 0; //creating count variable
+        String orf1 = ""; //creating empty orf1 string to store each codon
+        for (int i = 0; i < dna.length();i++){ //for loop iterates through the length of the dna string
+                if (count == 0) { //this if statement is needed so that the elif statement below isn't evaluated to true at count = 0
+                    orf1 += dna.charAt(i); //appending the char at the specified index
+                    count++; // incrementing count
+                }
+                else if (count%3 == 0){ //checking to see if count is divisible by 3 with remainder 0
+                    if ((dna.length()-count) < 3){ //checking to see if there are less than 3 nucleotides after the previous codon
+                        break; //breaking the loop if above is true
+                    }
+                    orf1 += " "; // adding a whitespace to the string to separate codons in the orf1 string
+                    orf1 += dna.charAt(i); //adding nucleotide to orf1
+                    count++; // incrementing count
+                }
+
+                else {
+                    orf1 += dna.charAt(i); //adding nucleotide to orf1
+                    count++; // incrementing count
+                }
+        }
+        System.out.print("\nReading Frame #1 codons are:\n"+orf1+"\n"); //printing orf1 with appropriate message
+
+        //The segment below will find orf2
+
+        count = 0; //reassigning count value to 0
+        String orf2 = ""; //creating orf2 string
+        for (int i = 1; i < dna.length();i++){ //for orf2, we are starting at i = 1
+            if (count == 0) {
+                orf2 += dna.charAt(i);
+                count++;
+            }
+            else if (count%3 == 0){
+                if ((dna.length()-count) < 3){
+                    break;
+                }
+                orf2 += " ";
+                orf2 += dna.charAt(i);;
+                count++;
+            }
+
+            else {
+                orf2 += dna.charAt(i);
+                count++;
+            }
+        }
+        System.out.print("\nReading Frame #2 codons are:\n"+orf2+"\n"); //printing orf2 with appropriate message
+
+        //The segment below will find orf3
+
+        count = 0; //reassigning count value to 0
+        String orf3 = ""; //creating orf3 string
+        for (int i = 2; i < dna.length();i++){ //for orf3, we are starting at i = 2
+
+            if (count == 0) {
+                orf3 += dna.charAt(i);
+                count++;
+            }
+            else if (count%3 == 0){
+                if ((dna.length()-count) < 3){
+                    break;
+                }
+                orf3 += " ";
+                orf3 += dna.charAt(i);;
+                count++;
+            }
+
+            else {
+                orf3 += dna.charAt(i);
+                count++;
+            }
+        }
+        System.out.print("\nReading Frame #3 codons are:\n"+orf3+"\n"); //printing orf3 with appropriate message
+
+
+    }
+
+    public static void codon2aa(String dna){ //creating codon2aa method -- this method a modified version of the method above
+
+        Dictionary aa_dict = new Hashtable(); //instantiating the dictionary class
+
+        //adding the key-value pairs for each codon and the amino acid it codes for to the aa_dict dictionary
+
+        aa_dict.put("GCT","A"); aa_dict.put("GCC","A"); aa_dict.put("GCA","A"); aa_dict.put("GCG","A"); aa_dict.put("CGT","R"); aa_dict.put("CGC","R"); aa_dict.put("CGA","R"); aa_dict.put("CGC","R"); aa_dict.put("CGG","R");
+        aa_dict.put("AGA","R"); aa_dict.put("AGG","R"); aa_dict.put("AAT","N"); aa_dict.put("AAC","N"); aa_dict.put("GAT","D"); aa_dict.put("GAC","D"); aa_dict.put("TGT","C"); aa_dict.put("TGC","C"); aa_dict.put("CAA","Q");
+        aa_dict.put("CAG","Q"); aa_dict.put("GAA","E"); aa_dict.put("GAG","E"); aa_dict.put("GGT","G"); aa_dict.put("GGC","G"); aa_dict.put("GGA","G"); aa_dict.put("GGG","G"); aa_dict.put("CAT","H"); aa_dict.put("CAC","H"); aa_dict.put("ATG","M"); aa_dict.put("ATT","I"); aa_dict.put("ATC","I");
+        aa_dict.put("ATA","I"); aa_dict.put("CTT","L"); aa_dict.put("CTC","L"); aa_dict.put("CTA","L"); aa_dict.put("CTG","L"); aa_dict.put("TTA","L"); aa_dict.put("TTG","L"); aa_dict.put("AAA","K"); aa_dict.put("AAG","K"); aa_dict.put("TTT","F");aa_dict.put("TTC","F"); aa_dict.put("CCT","P");
+        aa_dict.put("CCC","P");aa_dict.put("CCA","P"); aa_dict.put("CCG","P"); aa_dict.put("TCT","S");aa_dict.put("TCC","S"); aa_dict.put("TCA","S"); aa_dict.put("TCG","S");aa_dict.put("AGT","S"); aa_dict.put("AGC","S"); aa_dict.put("ACT","T");aa_dict.put("ACC","T"); aa_dict.put("ACA","T");
+        aa_dict.put("ACG","T");aa_dict.put("TGG","W"); aa_dict.put("TAT","Y"); aa_dict.put("TAC","Y");aa_dict.put("GTT","V"); aa_dict.put("GTC","V"); aa_dict.put("GTA","V");aa_dict.put("GTG","V"); aa_dict.put("TAA","*"); aa_dict.put("TGA","*");aa_dict.put("TAG","*");
+
+
+
+        System.out.println("\nThe DNA sequence is:\n"+dna);
+
+        //same as orf1 loop in codon() method above -- the segment for converting the codons to amino acids is after the first print statement
+
+        double count = 0;
+        String orf1 = "";
+        for (int i = 0; i < dna.length();i++){
+            if (count == 0) {
+                orf1 += dna.charAt(i);
+                count++;
+            }
+            else if (count%3 == 0){
+                if ((dna.length()-count) < 3){
+                    break;
+                }
+                orf1 += " ";
+                orf1 += dna.charAt(i);;
+                count++;
+            }
+
+            else {
+                orf1 += dna.charAt(i);
+                count++;
+            }
+        }
+        System.out.print("\nReading Frame #1 codons are:\n"+orf1+"\n");
+
+        String codon = ""; //creating an empty codon string variable
+        String aaChain1 = ""; //creating empty aaChain1 string variable
+        for(int i = 0; i < orf1.length();i = i + 4){ //iterating the length of orf1 but incrementing i by 4 each time (to skip over the whitespace)
+            codon += orf1.charAt(i); //appending char at index i to codon string (1st base of codon)
+            codon += orf1.charAt(i+1); //appending char at index i+1 to codon string (2nd base of codon)
+            codon += orf1.charAt(i+2); //appending char at index i+2 to codon string (3rd base of codon)
+
+            aaChain1 += aa_dict.get(codon); //appending the returned value of the codon key in the aa_dict dictionary
+            aaChain1 += "   "; //adding 3 spaces for formatting
+            codon = ""; //reassigning an empty string to codon for future iterations
+
+            }
+        System.out.print(aaChain1); //printing aaChain1 below the orf1 codons
+
+        //same as orf2 loop in codon() method above -- the segment for converting the codons to amino acids is after the first print statement
+
+        count = 0;
+        String orf2 = "";
+        for (int i = 1; i < dna.length();i++){
+            if (count == 0) {
+                orf2 += dna.charAt(i);
+                count++;
+            }
+            else if (count%3 == 0){
+                if ((dna.length()-count) < 3){
+                    break;
+                }
+                orf2 += " ";
+                orf2 += dna.charAt(i);;
+                count++;
+            }
+
+            else {
+                orf2 += dna.charAt(i);
+                count++;
+            }
+        }
+        System.out.print("\n\nReading Frame #2 codons are:\n"+orf2+"\n");
+
+        //same process as aaChain1 loop
+
+        String codon2 = "";
+        String aaChain2 = "";
+        for(int i = 0; i < orf2.length();i = i + 4){
+            codon2 += orf2.charAt(i);
+            codon2 += orf2.charAt(i+1);
+            codon2 += orf2.charAt(i+2);
+
+            aaChain2 += aa_dict.get(codon2);
+            aaChain2 += "   ";
+            codon2 = "";
+
+        }
+        System.out.print(aaChain2);
+
+        //same as orf3 loop in codon() method above -- the segment for converting the codons to amino acids is after the first print statement below
+
+        count = 0;
+        String orf3 = "";
+        for (int i = 2; i < dna.length();i++){
+            if (count == 0) {
+                orf3 += dna.charAt(i);
+                count++;
+            }
+            else if (count%3 == 0){
+                if ((dna.length()-count) < 3){
+                    break;
+                }
+                orf3 += " ";
+                orf3 += dna.charAt(i);;
+                count++;
+            }
+
+            else {
+                orf3 += dna.charAt(i);
+                count++;
+            }
+        }
+        System.out.print("\n\nReading Frame #3 codons are:\n"+orf3+"\n");
+
+        //same process as aaChain1 loop
+
+        String codon3 = "";
+        String aaChain3 = "";
+        for(int i = 0; i < orf3.length();i = i + 4){
+            codon3 += orf3.charAt(i);
+            codon3 += orf3.charAt(i+1);
+            codon3 += orf3.charAt(i+2);
+
+            aaChain3 += aa_dict.get(codon3);
+            aaChain3 += "   ";
+            codon3 = "";
+
+        }
+        System.out.print(aaChain3);
+
+    }
+
+}
---
+++ src/hwk3_1.java
@@ -0,0 +1,41 @@
+/*
+Class Name: BIFS 618
+Homework assignment3, question1
+File name: hwk3_1.java
+Program author name: Samuel Rusher
+*/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.Scanner;
+
+public class hwk3_1 {
+
+
+    public static void main(String[] args){
+
+        DNA codongen = new DNA(); // instantiating the DNA class in codongen
+
+        Scanner scanner1 = new Scanner(System.in); // creating scanner class
+        System.out.println("Please enter the file name of the DNA sequence: "); //prompting user for file name
+        try {
+            File myFile = new File(scanner1.nextLine()); //instantiating File class from the user's input
+            Scanner scanner2 = new Scanner(myFile); //scanner is reading myFile
+            while (scanner2.hasNextLine()){ //while loop is using scanner2 and hasNextLine() method to read through each line in the file, if there are no more lines, the loop terminates
+                codongen.dnaseq += scanner2.nextLine();
+            }
+            scanner2.close(); //closing scanenr
+        }
+        catch (FileNotFoundException e) { //catch block is looking for the FileNotFoundException, it will display the appropriate error message if the exception occurs
+            System.out.println("ERROR: The file you entered does not exist in the current directory.");
+            e.printStackTrace();
+        }
+
+        codongen.codon(codongen.dnaseq); //calling codon() method from the DNA class, with the dnaseq field as the parameter
+
+        codongen.codon2aa(codongen.dnaseq); //calling codon2aa() method from the DNA class, with the dnaseq field as the parameter
+
+
+    }
+}
